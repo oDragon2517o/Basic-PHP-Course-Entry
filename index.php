1b93bd5ae6519a639864720a540ce8adc500f1a9
Dragon2517 <rabvasya1994@yandex.ru>
2022-06-02 02:39:54 +0300

    1 задание

--- index.php
+++ index.php
@@ -1,11 +1,31 @@
-<h1>Тестовый заголовок</h1>
+<!-- <h1>Тестовый заголовок</h1>
 <?php
-echo "<h1>Добрый день! Сегодня ".date("d.m.Y")."</h1>"
+echo "<h1>Добрый день! Сегодня " . date("d.m.Y") . "</h1>"
 ?>
 
-<h1> Длюрый жень! Сегодня <?= date("d.m.Y")?></h1>
+<h1> Длюрый жень! Сегодня <?= date("d.m.Y") ?></h1>
 <br>
 <?php
 echo round(2.49);
 echo "<br>";
 echo "わたしわヴァシャです";
+
+
+
+
+?> -->
+
+<?php
+$title = "test";
+
+$god = date("Y");
+
+echo "<h1>" . " $title" . " $god" . "</h1>";
+?>
+
+
+<p>
+<form action="ДЗ/index.php" method="POST">
+    <input type="submit" value="ДЗ">
+</form>
+</p>
